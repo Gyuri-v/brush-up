52f15a3b0d64f0b92d4ec99455d810b80949cc4e
GYURI <fnffkdll@naver.com>
2022-04-19 23:41:11 +0900

    react-todo : useRef를 사용하여 인풋값 data.json 전달까지

--- react-todo/src/component/Home.js
+++ react-todo/src/component/Home.js
@@ -1,27 +1,52 @@
-import { useEffect, useState } from 'react';
-import dummy from '../db/data.json';
+import { useEffect, useState, useRef } from 'react';
+
+// ****** 잘 모르는 부분
+// 1. async, await 구문
+// 2. useRef, POST 부분 다시 복습해보기
 
 function Home() {
   const [todos, setTodos] = useState([]);
   const getTest = async () => {
     const json = await (await fetch(`http://localhost:3001/todos`)).json();
-    console.log(json);
     setTodos(json);
-    console.log(todos);
   };
   useEffect(() => {
     getTest();
   }, []);
+
+  const newTodoRef = useRef(null);
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    console.log(newTodoRef.current.value);
+
+    fetch(`http://localhost:3001/todos`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        todo: newTodoRef.current.value,
+      }),
+    }).then((response) => {
+      if (response.ok) {
+        alert('생성이 완료 되었습니다.');
+      }
+    });
+  }
+
   return (
     <div className="container">
       <h1 className="title">To Do List</h1>
       <div className="contents">
         <div className="write-wrap">
-          <form action="" id="write-form">
+          <form id="write-form" onSubmit={onSubmit}>
             <input
               type="text"
               max="25"
               placeholder="오늘 할 일을 입력해 주세요."
+              ref={newTodoRef}
               required
             />
             <button>추가</button>
